d7b04994f9c76b9170d46cbf4d1838f4739de470
MartinOrdonoCaro <martinordonocaro@gmail.com>
2019-03-06 11:00:19 +0100

    RequestRepositository - find by brotherhood

--- Acme-Madruga/src/main/java/repositories/RequestRepository.java
+++ Acme-Madruga/src/main/java/repositories/RequestRepository.java
@@ -1,8 +1,16 @@
+
 package repositories;
 
-import domain.Request;
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import domain.Request;
 
 public interface RequestRepository extends JpaRepository<Request, Integer> {
+
+	@Query("select r from Request r join r.procession p where p.brotherhood.id = ?1")
+	Collection<Request> findRequestByBrotherhood(int brotherhoodId);
 
 }
