a38bd6afc02fd3cb4687360322c1ee82fb08c413
MartinOrdonoCaro <martinordonocaro@gmail.com>
2019-03-06 11:18:47 +0100

    Reques controller - metodos borrados

--- Acme-Madruga/src/main/java/controllers/RequestController.java
+++ Acme-Madruga/src/main/java/controllers/RequestController.java
@@ -9,9 +9,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.TypeMismatchException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.Assert;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -108,62 +106,6 @@ public class RequestController extends AbstractController {
 				//				System.out.println(oops.getClass());
 				//				System.out.println(oops.getCause());
 				result = this.createEditModelAndView(request, "request.commit.error");
-			}
-		}
-		return result;
-	}
-
-	// Edit Request GET------------------------------------------------------------------------------------
-	@RequestMapping(value = "/brotherhood/edit", method = RequestMethod.GET)
-	public ModelAndView edit(@RequestParam final int requestId) {
-		ModelAndView result;
-		Request request;
-
-		try {
-			request = this.requestService.findOne(requestId);
-			Assert.notNull(request);
-		} catch (final Throwable oops) {
-			result = this.forbiddenOpperation();
-			return result;
-		}
-
-		result = this.editModelAndView(request);
-
-		return result;
-	}
-
-	// Edit Request POST------------------------------------------------------------------------------------
-	@RequestMapping(value = "/brotherhood/edit", method = RequestMethod.POST, params = "save")
-	public ModelAndView saveEdit(final Request request, final BindingResult binding) {
-		ModelAndView result;
-
-		if (binding.hasErrors()) {
-			final List<ObjectError> errors = binding.getAllErrors();
-			for (final ObjectError e : errors) {
-				System.out.println(e.toString());
-			}
-
-			result = new ModelAndView("request/member/create");
-			result.addObject("request", request);
-		} else {
-			try {
-				/*** Descomentar cuando el metodo funcione, comprobar que se manda la notificacion ***/
-				//final Request old = this.requestService.findOne(request.getId());
-				this.requestService.save(request);
-				//this.requestService.automaticNotification(request, old);
-				result = new ModelAndView("request/list");
-			} catch (final Throwable oops) {
-				System.out.println(request);
-				System.out.println(oops.getMessage());
-				System.out.println(oops.getClass());
-				System.out.println(oops.getCause());
-				result = this.createEditModelAndView(request);
-
-				if (oops instanceof DataIntegrityViolationException) {
-					result = this.createEditModelAndView(request, "request.commit.username");
-				} else {
-					result = this.createEditModelAndView(request, "request.commit.error");
-				}
 			}
 		}
 		return result;
